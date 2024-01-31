531e6f4b2ad5937e69e17de2abc986e4cf8b1362
Emre3Ayar <73703934+Emre3Ayar@users.noreply.github.com>
2024-01-31 19:51:04 +0100

    LithiumManager
    Added navigation, getting data from the backend(services), installed bootstrap, navbar component

--- LitiumManager/litium-manager/src/pages/home.tsx
+++ LitiumManager/litium-manager/src/pages/home.tsx
@@ -1,19 +1,46 @@
 //imports
 import React, { useEffect, useState, useRef } from 'react';
+import GetBatteriesRequest, {GetBatterieRequest} from '../services/BatteryServices';
+import Navbar from '../components/Navbar';
 //import ReactDOM from 'react-dom';
 const TemplatePage = () => {
     //const html = fs.readFileSync('/webpage.html', {encoding: 'utf8'});
-
+    useEffect(() => {
+      LoadData();
+    }, []);
+    const [selectedItems, setSelectedItems] = useState<any[]>();
+    const [items, setItems] = useState<any[]>();
+    const LoadData = async () => {
+      const allBattery = await GetBatteriesRequest();
+      setItems(allBattery);
+    }
+    const searchFunction = (id: number) => {
+      const battery = GetBatterieRequest(id);
+      if (!battery) {
+        setSelectedItems(battery);
+      }
+    }
+    let card = [];
     return (
         <>
-            <div style={{ marginLeft: '30%', marginTop: '10%', marginRight: '30%' }}>
-                <h1 className='text-monospace'>
-                    <b>Welcome to the E-mail Template Editor</b>
-                </h1>
-                <div>
-                test
-                </div>
+          <Navbar />
+            <div style={{ marginLeft: '2%', marginRight: '2%'  }}>
+            <h1>Battery list</h1>
+              <div className="row">
+                <div className="col-4">
+                  <div className="d-grid gap-2 col-6 mx-auto">
+                  {items?.map((number) => (
+                    <button onClick={() => GetBatterieRequest(number.id)} key={number.id} className="btn btn-primary" type="button">{number.batteryCode}</button>
+                  ))}
+                  </div>
             </div>
+          </div>
+          {selectedItems == null ? (
+            <div></div>
+          ) : (
+            <div>test</div>
+          )}
+        </div>
         </>
     );
 };
---
+++ LitiumManager/litium-manager/src/services/BatteryServices.tsx
@@ -0,0 +1,19 @@
+export default async function GetBatteriesRequest(){
+  const myHeaders = new Headers();
+  const requestOptions = {
+    method : "GET",
+    headers: myHeaders,
+  };
+  const resp = await fetch("http://localhost:8080/api/v1/batteries/JSON", requestOptions).then((response) => response.json()).then((result) => result);
+  return resp;
+};
+export async function GetBatterieRequest(searchTerm: number){
+  console.log(searchTerm);
+  const myHeaders = new Headers();
+  const requestOptions = {
+    method : "GET",
+    headers: myHeaders,
+  };
+  const resp = await fetch("http://localhost:8080/api/v1/batteries/get/" + searchTerm, requestOptions).then((response) => response.json()).then((result) => result);
+  return resp;
+};
